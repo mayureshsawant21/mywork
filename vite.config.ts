f0d0cca566905eb6fd8bf9e7ec62db51e5b145ab
mayureshsawant21 <sawantmayuresh2195@gmail.com>
2026-06-25 00:44:47 +0530

    Commit Replace

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: process.env.GITHUB_ACTIONS === 'true' ? '/mywork/' : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
